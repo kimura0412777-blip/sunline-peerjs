75f5c855efcee82875cc854c4eb010e280746028
kimura0412777-blip <kimura0412777@gmail.com>
2026-04-30 04:08:52 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ const port = process.env.PORT || 9000;
 const server = PeerServer({
   port: port,
   path: "/peerjs",
-  allow_discovery: true,
-  alive_timeout: 60000,
-  key: "sunline",
 });
 
 server.on("connection", (client) => {
   console.log("[CONNECT]", client.getId());
 });
+
 server.on("disconnect", (client) => {
   console.log("[DISCONNECT]", client.getId());
 });
 
-console.log("✅ PeerJS server running on port " + port + " path /peerjs");
+console.log("PeerJS server running on port " + port);
